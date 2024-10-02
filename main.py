89a2ac08f3f10e118c9df1f11cbf028eee28e317
lauralstephenson <lstephenson556@gmail.com>
2024-10-02 17:28:54 +0900

    This uploads the folder to GitHub.

---
+++ main.py
@@ -0,0 +1,6 @@
+age = input("What is your age?")
+# 🚨 Don't change the code above 👆
+# Write your code below this line 👇
+years = 90 - int(age)
+weeks_left = int(years * 52)
+print(f"You have {weeks_left} weeks left.")
